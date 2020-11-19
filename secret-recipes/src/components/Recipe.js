808fc3e007dfda8b508b8558aefc5bdef44b4ca4
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-11-18 19:15:17 -0500

    clearned some code

---
+++ secret-recipes/src/components/Recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Recipe(props) {
+	return (
+		<div>
+			<img />
+			<h3>title</h3>
+			<p>Author</p>
+			<p>ingredients:s</p>
+			<p>instructions:</p>
+		</div>
+	);
+}
+
+export default Recipe;
